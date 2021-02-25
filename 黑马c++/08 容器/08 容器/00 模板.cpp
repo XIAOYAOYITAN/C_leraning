38e458ecad2272b5b82d4c97cad8a2bb9fbf1f32
xiaoyao <laihangliang@163.com>
2021-02-25 18:37:39 +0800

    2021.2.25

--- 黑马c++/08 容器/08 容器/00 模板.cpp
+++ 黑马c++/08 容器/08 容器/00 模板.cpp
@@ -1,9 +1,102 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//int main() {
-//
-//	system("pause");
-//	return 0;
-//}
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <ctime>
+using namespace std;
+
+#define CEHUA  0
+#define MEISHU 1
+#define YANFA  2
+
+
+//•	公司今天招聘了10个员工（ABCDEFGHIJ），10名员工进入公司之后，需要指派员工在那个部门工作
+//•	员工信息有 : 姓名 工资组成；部门分为：策划、美术、研发
+//•	随机给10名员工分配部门和工资
+//•	通过multimap进行信息的插入 key(部门编号) value(员工)
+//•	分部门显示员工信息
+
+class Worker {
+public:
+	Worker(string name, int salary) {
+		this->m_name = name;
+		this->m_salary = salary;
+	}
+public:
+	string m_name;
+	int m_salary;
+};
+
+void createWorker(vector<Worker> &v) {
+
+	string NameSeed = "ABCDEFGHIJ";
+		for (int i = 0; i < 10; i++)
+		{
+			string name = "员工";
+			name += NameSeed[i];
+			
+			int salary = rand() % 10000 + 10000;//10000~19999
+			Worker w(name, salary);
+			v.push_back(w);
+		}
+}
+
+void setGroup(vector<Worker> &v, multimap<int, Worker> &map) {
+	
+	for (vector<Worker>::iterator it = v.begin(); it != v.end(); it++) {
+		int groupname = rand() % 3;//0到2
+		map.insert(pair<int, Worker>(groupname, *it));
+	}
+
+}
+
+void showGroup(multimap<int, Worker> &map) {
+
+	multimap<int, Worker>::iterator pos=map.find(CEHUA);
+	int count=0;
+	for (; pos != map.end()&&count<map.count(CEHUA); pos++,count++) {
+		cout << "策划部门有：" << endl;
+		cout << "员工姓名：" << pos->second.m_name << "  员工薪资：" << pos->second.m_salary << endl;
+	}
+
+	cout << "---------------------------------" << endl;
+	pos = map.find(MEISHU);
+    count = 0;
+	for (; pos != map.end() && count < map.count(MEISHU); pos++, count++) {
+		cout << "美术部门有：" << endl;
+		cout << "员工姓名：" << pos->second.m_name << "  员工薪资：" << pos->second.m_salary << endl;
+	}
+
+	cout << "---------------------------------" << endl;
+	pos = map.find(YANFA);
+	count = 0;
+	for (; pos != map.end() && count < map.count(YANFA); pos++, count++) {
+		cout << "研发部门有：" << endl;
+		cout << "员工姓名：" << pos->second.m_name << "  员工薪资：" << pos->second.m_salary << endl;
+	}
+
+}
+
+int main() {
+	vector<Worker> vWorker;
+	multimap<int, Worker> wMap;
+
+	srand((unsigned int)time(NULL));
+	//1. 创建10名员工，并对工资进行随机分配
+	createWorker(vWorker);
+	
+	////打印测试
+	//for (vector<Worker>::iterator it = vWorker.begin(); it != vWorker.end(); it++) {
+	//
+	//	cout << "员工姓名：" << it->m_name << "  员工薪资：" << it->m_salary << endl;
+	//}
+
+	//2. 对员工进行分组
+	setGroup(vWorker,wMap);
+
+	//3.对分组结果进行展示
+	showGroup(wMap);
+
+	system("pause");
+	return 0;
+}
---
+++ 黑马c++/08 容器/08 容器/11 set容器排序.cpp
@@ -0,0 +1,55 @@
+//#include <iostream>
+//#include <string>
+//#include <set>
+//#include <string>
+//using namespace std;
+//
+//class Person {
+//
+//public:
+//	Person(string name,int age) {
+//		this->m_name = name;
+//		this->m_age = age;
+//	}
+//	string m_name;
+//	int m_age;
+//};
+//
+//class myCompare {
+//
+//public:
+//	bool operator()(const Person &p1, const Person &p2) {
+//	
+//		return p1.m_age < p2.m_age;
+//
+//	}
+//
+//};
+//
+//void test01() {
+//
+//	set<Person,myCompare> s;
+//
+//	Person p1("刘备",24);
+//	Person p2("关羽", 32);
+//	Person p3("张飞", 27);
+//	Person p4("赵云", 21);
+//
+//	s.insert(p1);
+//	s.insert(p2);
+//	s.insert(p3);
+//	s.insert(p4);
+//
+//	for (set<Person, myCompare>::iterator it = s.begin(); it != s.end(); it++) {
+//	
+//		cout << "姓名为："<<(*it).m_name<<" 年龄为：" <<(*it).m_age<< endl;
+//	}
+//
+//}
+//
+//int main() {
+//
+//	test01();
+//	system("pause");
+//	return 0;
+//}
